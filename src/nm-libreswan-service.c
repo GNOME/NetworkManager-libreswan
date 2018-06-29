839049cf9b9a8693757084df04d437c55f5f463c
Francesco Giudici <fgiudici@redhat.com>
2018-06-29 18:40:08 +0200

    all: add support to IKEv2
    Add the new option 'ikev2' mapping the libreswan config one. When the
    option is found and enables IKEv2, skip xauth parameters (and add the
    'ikev2' option itself of course).
    The configuration NM side will be a IRAC (IPsec Remote Access Client)
    one because CP mode will be enforced but no EAP method will be used
    (as it is not yet available in libreswan too).

--- src/nm-libreswan-service.c
+++ src/nm-libreswan-service.c
@@ -260,6 +260,7 @@ static ValidProperty valid_properties[] = {
 	{ NM_LIBRESWAN_SALIFETIME,                 G_TYPE_STRING, 0, 0 },
 	{ NM_LIBRESWAN_VENDOR,                     G_TYPE_STRING, 0, 0 },
 	{ NM_LIBRESWAN_REMOTENETWORK,              G_TYPE_STRING, 0, 0 },
+	{ NM_LIBRESWAN_IKEV2,                      G_TYPE_STRING, 0, 0 },
 	/* Ignored option for internal use */
 	{ NM_LIBRESWAN_PSK_INPUT_MODES,            G_TYPE_NONE, 0, 0 },
 	{ NM_LIBRESWAN_XAUTH_PASSWORD_INPUT_MODES, G_TYPE_NONE, 0, 0 },
