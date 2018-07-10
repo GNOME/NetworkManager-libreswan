b19a6a13d8fcfd28524f4f9967560249f5e21213
Francesco Giudici <fgiudici@redhat.com>
2018-07-10 15:58:13 +0200

    all: add support to more Libreswan options
    Add support to the 'rightid', 'narrowing', 'rekey', 'fragmentation' and
    'mobike' Libreswan options

--- shared/utils.c
+++ shared/utils.c
@@ -106,6 +106,12 @@ nm_libreswan_config_write (gint fd,
 	const char *leftid;
 	const char *remote_network;
 	const char *ikev2;
+	const char *rightid;
+	const char *narrowing;
+	const char *rekey;
+	const char *fragmentation;
+	const char *mobike;
+	gboolean is_ikev2 = FALSE;
 	gboolean has_xauth = TRUE;
 
 	g_return_val_if_fail (fd > 0, FALSE);
@@ -118,6 +124,7 @@ nm_libreswan_config_write (gint fd,
 
 	ikev2 = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_IKEV2);
 	if (NM_IN_STRSET (ikev2, "propose", "yes", "insist")) {
+		is_ikev2 = TRUE;
 		has_xauth = FALSE;
 	}
 	leftid = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_LEFTID);
@@ -145,6 +152,9 @@ nm_libreswan_config_write (gint fd,
 		WRITE_CHECK (fd, debug_write_fcn, error, " leftupdown=%s", leftupdown_script);
 
 	WRITE_CHECK (fd, debug_write_fcn, error, " right=%s", nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_RIGHT));
+	rightid = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_RIGHTID);
+	if (rightid && strlen (rightid))
+		WRITE_CHECK (fd, debug_write_fcn, error, " rightid=%s", rightid);
 	WRITE_CHECK (fd, debug_write_fcn, error, " rightmodecfgserver=yes");
 	WRITE_CHECK (fd, debug_write_fcn, error, " modecfgpull=yes");
 
@@ -182,8 +192,6 @@ nm_libreswan_config_write (gint fd,
 	else
 		WRITE_CHECK (fd, debug_write_fcn, error, " phase2alg=%s", phase2_alg_str);
 
-	WRITE_CHECK (fd, debug_write_fcn, error, " rekey=yes");
-
 	phase1_lifetime_str = nm_setting_vpn_get_data_item (s_vpn,
 							    NM_LIBRESWAN_IKELIFETIME);
 	if (!phase1_lifetime_str || !strlen (phase1_lifetime_str))
@@ -200,13 +208,30 @@ nm_libreswan_config_write (gint fd,
 		WRITE_CHECK (fd, debug_write_fcn, error, " salifetime=%s",
 			     phase2_lifetime_str);
 
-	WRITE_CHECK (fd, debug_write_fcn, error, " keyingtries=1");
+	rekey = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_REKEY);
+	if (!rekey || !strlen (rekey)) {
+		WRITE_CHECK (fd, debug_write_fcn, error, " rekey=yes");
+		WRITE_CHECK (fd, debug_write_fcn, error, " keyingtries=1");
+	} else
+		WRITE_CHECK (fd, debug_write_fcn, error, " rekey=%s", rekey);
 
 	if (!openswan && g_strcmp0 (nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_VENDOR), "Cisco") == 0)
 		WRITE_CHECK (fd, debug_write_fcn, error, " cisco-unity=yes");
 
 	if (ikev2 && strlen (ikev2))
 		WRITE_CHECK (fd, debug_write_fcn, error, " ikev2=%s", ikev2);
+
+	narrowing = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_NARROWING);
+	if (narrowing && strlen (narrowing))
+		WRITE_CHECK (fd, debug_write_fcn, error, " narrowing=%s", narrowing);
+
+	fragmentation = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_FRAGMENTATION);
+	if (fragmentation && strlen (fragmentation))
+		WRITE_CHECK (fd, debug_write_fcn, error, " fragmentation=%s", fragmentation);
+
+	mobike = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_MOBIKE);
+	if (mobike && strlen (mobike))
+		WRITE_CHECK (fd, debug_write_fcn, error, " mobike=%s", mobike);
 
 	WRITE_CHECK_NEWLINE (fd, trailing_newline, debug_write_fcn, error, " auto=add");
 
