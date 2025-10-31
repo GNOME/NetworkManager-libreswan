GNOME/NetworkManager-libreswan

commit baf90380d7f63c994476a365288d85a23007b915

shared: make ipsec.conf parsing declarative
Reuse metadata used for producing ipsec.conf fragments to drive the
import of ipsec.conf fragments and ensure their soundness.

This makes import much more sensible: it now deals with quoting
properly, doesn't ignore errors silently. It also validates the handful
of properties we decided to be smart about and generate them, but not
allow the user to set them explicitly: it just checks whether what we'd
generate is the same as specified in input, and happily proceed if it
matches and fail otherwise.

Also, cover it with tests, like a real grownup.

https://gitlab.gnome.org/GNOME/NetworkManager-libreswan/-/merge_requests/38

shared/utils.c
@@ -408,12 +408,10 @@ sanitize_setting_vpn (NMSettingVpn *s_vpn,
 
 		keys = nm_setting_vpn_get_data_keys (s_vpn, &length);
 		for (i = 0; i < length; i++) {
-			if (   (params[i].flags & PARAM_SYNTHETIC) == 0
-			    && nm_setting_vpn_get_data_item (sanitized, keys[i])) {
+			if (nm_setting_vpn_get_data_item (sanitized, keys[i]))
 				continue;
-			}
 
-		        g_set_error (error,
+			g_set_error (error,
 			             NM_UTILS_ERROR,
 			             NM_UTILS_ERROR_INVALID_ARGUMENT,
 			             _("property '%s' invalid or not supported"),
@@ -483,6 +481,157 @@ nm_libreswan_get_ipsec_conf (int ipsec_version,
 	}
 
 	return g_string_free (g_steal_pointer (&ipsec_conf), FALSE);
+}
+
+/*
+ * The format as described in ipsec.conf(5) is fairly primitive.
+ * In values, no line breaks are allowed. If there's other whitespace,
+ * it needs to be enclosed in quote marks. Quote marks are not allowed
+ * elsewhere. There's no escaping of the quote marks or newlines or
+ * anything else. This makes it feasible to parse it with a fairly
+ * regexp.
+ */
+static const char line_match[] =
+	"^(?:"
+	    "(?:conn\\s+|\\s+(\\S+)\\s*=\\s*)"	/* <"conn "> or <whitespace><key>...=... */
+	    "(?:\"([^\"]*)\"|(\\S+))"		/* <value> or "<v a l u e>" */
+	")?"					/* (or just blank line) */
+	"\\s*(?:#.*)?$";			/* optional comment */
+
+NMSettingVpn *
+nm_libreswan_parse_ipsec_conf (const char *ipsec_conf,
+                               char **out_con_name,
+                               GError **error)
+{
+	gs_unref_object NMSettingVpn *sanitized = NULL;
+	gs_unref_object NMSettingVpn *s_vpn = NULL;
+	gs_strfreev char **lines = NULL;
+	gs_free char *con_name = NULL;
+	GMatchInfo *match_info = NULL;
+	GError *parse_error = NULL;
+	GRegex *line_regex;
+	const char *old, *new;
+	const char *rekey;
+	char *key, *val;
+	int i;
+
+	g_return_val_if_fail (ipsec_conf, NULL);
+	g_return_val_if_fail (out_con_name && !*out_con_name, NULL);
+	g_return_val_if_fail (!error || !*error, NULL);
+
+	line_regex = g_regex_new (line_match, G_REGEX_RAW, 0, NULL);
+	g_return_val_if_fail (line_regex, NULL);
+
+	s_vpn = NM_SETTING_VPN (nm_setting_vpn_new ());
+
+	lines = g_strsplit_set (ipsec_conf, "\r\n", -1);
+	for (i = 0; lines[i]; i++) {
+		if (!g_regex_match (line_regex, lines[i], 0, &match_info)) {
+			parse_error = g_error_new (
+				NM_UTILS_ERROR,
+				NM_UTILS_ERROR_INVALID_ARGUMENT,
+				_("'%s' not understood"),
+				lines[i]);
+			g_match_info_unref (match_info);
+			break;
+		}
+
+		key = g_match_info_fetch (match_info, 1); /* Key */
+		val = g_match_info_fetch (match_info, 2); /* Unquoted value */
+		if (val[0] == '\0') {
+			g_free (val);
+			/* Quoted value (quotes stripped off) */
+			val = g_match_info_fetch (match_info, 3);
+		}
+		g_match_info_unref (match_info);
+
+		if (key[0] != '\0') {
+			/* key=value line */
+			if (con_name == NULL) {
+				parse_error = g_error_new (
+					NM_UTILS_ERROR,
+					NM_UTILS_ERROR_INVALID_ARGUMENT,
+					_("Expected a conn line before '%s'"),
+					key);
+			} else if (nm_setting_vpn_get_data_item (s_vpn, key)) {
+				parse_error = g_error_new (
+					NM_UTILS_ERROR,
+					NM_UTILS_ERROR_INVALID_ARGUMENT,
+					_("'%s' specified multiple times"),
+					key);
+			} else {
+				nm_setting_vpn_add_data_item (s_vpn, key, val);
+			}
+			g_free (key);
+			g_free (val);
+		} else if (val[0] != '\0') {
+			/* If key didn't match, then this must be a "conn" line. */
+			g_free (key);
+			if (con_name != NULL) {
+				g_free (val);
+				parse_error = g_error_new (
+					NM_UTILS_ERROR,
+					NM_UTILS_ERROR_INVALID_ARGUMENT,
+					_("'%s' specified multiple times"),
+					"conn");
+			} else {
+				con_name = val;
+			}
+		} else {
+			/* Blank line */
+			g_free (key);
+			g_free (val);
+		}
+
+		if (parse_error)
+			break;
+	}
+	g_regex_unref (line_regex);
+	if (parse_error) {
+		g_propagate_error (error, parse_error);
+		return NULL;
+	}
+
+	/* The "keyingtries" kludge. See above. */
+	rekey = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_REKEY);
+	if (  rekey && rekey[0] != '\0'
+	    && g_strcmp0 (nm_setting_vpn_get_data_item (s_vpn, "keyingtries"), "1") == 0) {
+		nm_setting_vpn_remove_data_item (s_vpn, "keyingtries");
+	}
+
+	sanitized = sanitize_setting_vpn (s_vpn, error);
+	if (!sanitized)
+		return NULL;
+
+	g_return_val_if_fail (con_name, NULL);
+
+	/*
+	 * Verify that the synthetic properties are either not present in the
+	 * original connection, or have the same value as has been synthesized,
+	 * Then remove them.
+	 */
+	for (i = 0; params[i].name != NULL; i++) {
+		if ((params[i].flags & PARAM_SYNTHETIC) == 0)
+			continue;
+
+		old = nm_setting_vpn_get_data_item (s_vpn, params[i].name);
+		if (old != NULL) {
+			new = nm_setting_vpn_get_data_item (sanitized, params[i].name);
+			if (g_strcmp0 (old, new) != 0) {
+				g_set_error (error,
+				             NM_UTILS_ERROR,
+				             NM_UTILS_ERROR_INVALID_ARGUMENT,
+				             _("'%s' is not supported for '%s'"),
+				             old, params[i].name);
+				return NULL;
+			}
+		}
+
+		nm_setting_vpn_remove_data_item (sanitized, params[i].name);
+	}
+
+	*out_con_name = g_steal_pointer (&con_name);
+	return g_steal_pointer (&sanitized);
 }
 
 static const char *

shared/test-utils.c
@@ -197,12 +197,430 @@ test_config_write (void)
 	g_object_unref (s_vpn);
 }
 
+static void
+test_config_read (void)
+{
+	char *con_name = NULL;
+	GError *error = NULL;
+	NMSettingVpn *s_vpn;
+
+	/* Minimal. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" right=11.12.13.14\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	/* Also include all generated properties. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" salifetime=24h\n"
+		" rightsubnet=0.0.0.0/0\n"
+		" right=11.12.13.14\n"
+		" rekey=yes\n"
+		" leftmodecfgclient=yes\n"
+		" left=%defaultroute\n"
+		" ikev2=never\n"
+		" ikelifetime=24h\n"
+		" authby=secret\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	/* Include all synthetic properties with appropriate values. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+	        "conn xpl\n"
+	        " ikev2=never\n"
+	        " right=172.31.79.2\n"
+	        " leftid=@groupname\n"
+	        " left=%defaultroute\n"
+	        " leftmodecfgclient=yes\n"
+	        " authby=secret\n"
+	        " ike=aes256-sha1;modp1536\n"
+	        " ikelifetime=24h\n"
+	        " salifetime=24h\n"
+	        " rightsubnet=10.0.2.0/24\n"
+	        " leftusername=\"username\"\n"
+	        " rekey=yes\n"
+	        " keyingtries=1\n"
+	        " aggrmode=yes\n"
+	        " leftxauthclient=yes\n"
+	        " rightxauthserver=yes\n"
+	        " remote-peer-type=cisco\n"
+	        " rightmodecfgserver=yes\n"
+	        " modecfgpull=yes\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 12);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ike"),			==, "aes256-sha1;modp1536");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftid"),		==, "@groupname");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftusername"),		==, "username");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "172.31.79.2");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "10.0.2.0/24");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn con_name\n"
+		" ikev2=never\n"
+		" right=11.12.13.14\n"
+		" left=%defaultroute\n"
+		" leftmodecfgclient=yes\n"
+		" authby=secret\n"
+		" ikelifetime=24h\n"
+		" salifetime=24h\n"
+		" rightsubnet=0.0.0.0/0\n"
+		" rekey=yes\n"
+		" keyingtries=1\n"
+		" leftxauthclient=yes\n"
+		" rightxauthserver=yes\n"
+		" remote-peer-type=cisco\n"
+		" rightmodecfgserver=yes\n"
+		" modecfgpull=yes\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (con_name, ==, "con_name");
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		 "conn f0008435-07af-4836-a53d-b43e8730e68f\n"
+		 " ikev2=insist\n"
+		 " right=11.12.13.14\n"
+		 " leftcert=\"Libreswan Client\"\n"
+		 " rightrsasigkey=\"%cert\"\n"
+		 " leftrsasigkey=%cert\n"
+		 " left=%defaultroute\n"
+		 " leftmodecfgclient=yes\n"
+		 " rightsubnet=0.0.0.0/0\n"
+		 " rekey=yes\n"
+		 " phase2alg=aes256-sha1\n"
+		 " keyingtries=1\n"
+		 " rightmodecfgserver=yes\n"
+		 " modecfgpull=yes\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (con_name, ==, "f0008435-07af-4836-a53d-b43e8730e68f");
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "insist");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftcert"),		==, "Libreswan Client");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftrsasigkey"),		==, "%cert");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightrsasigkey"),	==, "%cert");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" ikev2=insist\n"
+		" right=11.12.13.14\n"
+		" rightrsasigkey=\"world\"\n"
+		" leftrsasigkey=\"hello\"\n"
+		" left=%defaultroute\n"
+		" leftmodecfgclient=yes\n"
+		" rightsubnet=0.0.0.0/0\n"
+		" rekey=yes\n"
+		" esp=aes256-sha1\n"
+		" keyingtries=1\n"
+		" rightmodecfgserver=yes\n"
+		" modecfgpull=yes\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (con_name, ==, "conn");
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "insist");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftrsasigkey"),		==, "hello");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightrsasigkey"),	==, "world");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn con_name\n"
+		" ikev2=never\n"
+		" right=11.12.13.14\n"
+		" left=%defaultroute\n"
+		" leftmodecfgclient=yes\n"
+		" authby=secret\n"
+		" ikelifetime=24h\n"
+		" rekey=yes\n"
+		" rightsubnet=0.0.0.0/0\n"
+		" salifetime=24h\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (con_name, ==, "con_name");
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"# This configuration was created unded influence.\r\n"
+		"# Do not edit!\n\r"
+		"\n"
+		"    # the # below doesn't introduce a comment  \n"
+		"conn con#name\n"
+		"# comments are preceded by whitespace\n"
+		"\t\n"
+		" ikev2=never\n"
+		" left=\t\t%defaultroute\n"
+		"\t# moo"
+		"\tleftmodecfgclient  = \t yes\n"
+		" authby=\"secret\"\n"
+		"# boo"
+		"    ikelifetime =24h   # what\n"
+		"  \t rekey=yes\n"
+		"\n"
+		" rightsubnet=\t0.0.0.0/0\n"
+		"   #wot\r"
+		" \tright=11.12.13.14\n"
+		" \t\tsalifetime=24h",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (con_name, ==, "con#name");
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 9);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	/* Make sure properties with right synthetic values are allowed. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" right=11.12.13.14\n"
+		" vendor=Cisco\n"
+		" cisco-unity=yes\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 10);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "vendor"),		==, "Cisco");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		" right=11.12.13.14\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		"right=11.12.13.14\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		" right=11.12.13.14\n"
+		"conn my_con\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		" right=11.12.13.14\n"
+		"conn my_con\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		" right=11.12.13.14\n"
+		" right=11.12.13.14\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	/* Not an actual property */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		" right=11.12.13.14\n"
+		" hola=prdel\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		" right=11.12.13.14\n"
+		" leftcert=Libreswan Client\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn my_con\n"
+		" ikev2=never\n"
+		" right=11.12.13.14\n"
+		" left=%defaultroute\n"
+		" leftmodecfgclient=yes\n"
+		" authby=secret\n"
+		" ikelifetime=24h\n"
+		" salifetime=24h\n"
+		" rightsubnet=0.0.0.0/0\n"
+		" rekey=yes\n"
+		" keyingtries=1\n"
+		" leftxauthclient=yes\n"
+		" rightxauthserver=yes\n"
+		" remote_peer_type=cisco\n"
+		" rightmodecfgserver=yes\n"
+		" modecfgpull=yes\n"
+		" leftupdown=\"/foo/bar/ifupdown hello 123 456\"\n"
+		" auto=add\n"
+		" nm-configured=yes",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	/* Make sure synthetic properties can't be overriden. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" right=11.12.13.14\n"
+		" rightmodecfgserver=no\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+
+	/* Make sure properties with right synthetic values not allowed when they
+	 * wouldn't be used. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" right=11.12.13.14\n"
+		" cisco-unity=yes\n",
+		&con_name,
+		&error);
+	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
+	g_assert_null (s_vpn);
+	g_assert_null (con_name);
+	g_clear_error (&error);
+}
+
 int
 main (int argc, char **argv)
 {
 	g_test_init (&argc, &argv, NULL);
 
 	g_test_add_func ("/utils/config/write", test_config_write);
+	g_test_add_func ("/utils/config/read", test_config_read);
 
 	return g_test_run ();
 }

commit 8510dd4ec1a82bde1231a4ce25ba93c4ed30b6cf

utils: fix regex for matching key=value lines
If there is a RSA key ending with a double equal sign:

  leftrsasigkey=0sAwEAA==

the current regex '\S+' for the key greedily matches up to the second to
last equal sign, giving

  key   : "leftrsasigkey=0sAwEAA"
  value : "="

Fix the regex to forbid equal signs in the key.

Fixes: baf90380d7f6 ("shared: make ipsec.conf parsing declarative")

shared/utils.c
@@ -596,7 +596,7 @@ nm_libreswan_check_value(const char *key, const char *val, GError **error)
  */
 static const char line_match[] =
 	"^(?:"
-	"(?:conn\\s+|\\s+(\\S+)\\s*=\\s*)" /* <"conn "> or <whitespace><key>...=... */
+	"(?:conn\\s+|\\s+([^\\s=]+)\\s*=\\s*)" /* <"conn "> or <whitespace><key>...=... */
 	"(?:\"([^\"]*)\"|(\\S+))"          /* <value> or "<v a l u e>" */
 	")?"                               /* (or just blank line) */
 	"\\s*(?:#.*)?$";                   /* optional comment */

shared/test-utils.c
@@ -794,6 +794,30 @@ test_config_read(void)
 	g_assert_null(s_vpn);
 	g_assert_null(con_name);
 	g_clear_error(&error);
+
+	/* Keys with embedded equal signs. The keys here are not valid keys, they were
+	 * shortened to fit a single line. */
+#define RSASIGKEY1 "0sAwEAAcy05zwQ9LMZrc9GrK4CQIfiw=="
+#define RSASIGKEY2 "0sAwEAAcrSZGhYW8ZJVzRG/3SYiAT9Q0="
+	s_vpn = nm_libreswan_parse_ipsec_conf("conn conn\n"
+	                                      " leftid=@west\n"
+	                                      " left=192.0.42.1\n"
+	                                      " leftrsasigkey=" RSASIGKEY1 "\n"
+	                                      " rightid=@east\n"
+	                                      " right=192.0.42.2\n"
+	                                      " rightrsasigkey=" RSASIGKEY2 "\n"
+	                                      " authby=rsasig\n",
+	                                      &con_name,
+	                                      &error);
+	g_assert_no_error(error);
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "leftid"), ==, "@west");
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "left"), ==, "192.0.42.1");
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "leftrsasigkey"), ==, RSASIGKEY1);
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "rightid"), ==, "@east");
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "right"), ==, "192.0.42.2");
+	g_assert_cmpstr(nm_setting_vpn_get_data_item(s_vpn, "rightrsasigkey"), ==, RSASIGKEY2);
+	g_object_unref(s_vpn);
+	g_clear_pointer(&con_name, g_free);
 }
 
 static void